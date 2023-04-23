24e0d7d54658fbe468cf0db2b787c88afac673c1
Pham Anh Tuan <phamtuan210394@gmail.com>
2023-04-23 14:41:59 +0700

    Update success layout

--- libs/onboarding/success/src/lib/onboarding-success.tsx
+++ libs/onboarding/success/src/lib/onboarding-success.tsx
@@ -1,4 +1,6 @@
-import { Box, Typography } from '@mui/material';
+import React from 'react';
+import { Box, Card, CardContent, Typography } from '@mui/material';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 /* eslint-disable-next-line */
 export interface OnboardingSuccessProps {}
@@ -11,9 +13,30 @@ export function OnboardingSuccess(props: OnboardingSuccessProps) {
       alignItems="center"
       sx={{ background: '#efeff1', padding: '2rem', minHeight: '100vh' }}
     >
-      <Typography variant="h4" align="center">
-        Thank you for providing your information!
-      </Typography>
+      <Card sx={{ minWidth: '60%', padding: '2rem' }}>
+        <CardContent>
+          <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <CheckCircleIcon
+              sx={{
+                fontSize: '5rem',
+                color: '#4caf50',
+                marginBottom: '1rem',
+              }}
+            />
+            <Typography variant="h4" align="center">
+              Thank you for providing your information!
+            </Typography>
+            <Typography variant="subtitle1" align="center">
+              We will review your information and get back to you shortly.
+            </Typography>
+          </Box>
+        </CardContent>
+      </Card>
     </Box>
   );
 }
