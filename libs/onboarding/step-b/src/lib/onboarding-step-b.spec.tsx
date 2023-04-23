c15e93892b939378ed3089b94ce81d6b8f396513
Pham Anh Tuan <phamtuan210394@gmail.com>
2023-04-23 17:56:32 +0700

    unit test step B and step c

--- libs/onboarding/step-b/src/lib/onboarding-step-b.spec.tsx
+++ libs/onboarding/step-b/src/lib/onboarding-step-b.spec.tsx
@@ -1,10 +1,74 @@
-import { render } from '@testing-library/react';
-
+import { render, fireEvent, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import OnboardingStepB from './onboarding-step-b';
+import '@testing-library/jest-dom';
 
 describe('OnboardingStepB', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<OnboardingStepB />);
-    expect(baseElement).toBeTruthy();
+    expect(baseElement).toBeInTheDocument();
+  });
+
+  it('should render the email field', () => {
+    render(<OnboardingStepB />);
+    const emailField = screen.getByLabelText('Email');
+    expect(emailField).toBeInTheDocument();
+  });
+
+  it('should render the phone number field', () => {
+    render(<OnboardingStepB />);
+    const phoneNumberField = screen.getByLabelText('Phone Number');
+    expect(phoneNumberField).toBeInTheDocument();
+  });
+
+  it('should render the date of birth field', () => {
+    render(<OnboardingStepB />);
+    const dateOfBirthField = screen.getByLabelText('Date of Birth');
+    expect(dateOfBirthField).toBeInTheDocument();
+  });
+
+  it('should render the next and back buttons', () => {
+    render(<OnboardingStepB />);
+    const nextButton = screen.getByRole('button', { name: 'Next' });
+    expect(nextButton).toBeInTheDocument();
+    const backButton = screen.getByRole('button', { name: 'Back' });
+    expect(backButton).toBeInTheDocument();
+  });
+
+  it('should show errors when submitting the form with empty fields', async () => {
+    render(<OnboardingStepB />);
+    const nextButton = screen.getByRole('button', { name: 'Next' });
+
+    fireEvent.click(nextButton);
+
+    const emailError = await screen.findByText('Email is required');
+    expect(emailError).toBeInTheDocument();
+
+    const phoneNumberError = await screen.findByText(
+      'Phone number is required'
+    );
+    expect(phoneNumberError).toBeInTheDocument();
+
+    const dateOfBirthError = await screen.findByText(
+      'Date of birth is required'
+    );
+    expect(dateOfBirthError).toBeInTheDocument();
+  });
+
+  it('should submit the form with valid data', async () => {
+    render(<OnboardingStepB />);
+    const emailField = screen.getByLabelText('Email');
+    const phoneNumberField = screen.getByLabelText('Phone Number');
+    const dateOfBirthField = screen.getByLabelText('Date of Birth');
+    const nextButton = screen.getByRole('button', { name: 'Next' });
+
+    userEvent.type(emailField, 'john.doe@example.com');
+    userEvent.type(phoneNumberField, '1234567890');
+    userEvent.type(dateOfBirthField, '1990-01-01');
+    fireEvent.click(nextButton);
+
+    // Replace the following line with a test that checks if the form submission was successful
+    // For example, check if the next step is rendered, or if the store has been updated with the submitted data
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // Wait for a second before checking the result
   });
 });
--- libs/onboarding/step-c/src/lib/onboarding-step-c.spec.tsx
+++ libs/onboarding/step-c/src/lib/onboarding-step-c.spec.tsx
@@ -1,10 +1,60 @@
-import { render } from '@testing-library/react';
-
+import { render, fireEvent, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import OnboardingStepC from './onboarding-step-c';
+import '@testing-library/jest-dom';
 
 describe('OnboardingStepC', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<OnboardingStepC />);
-    expect(baseElement).toBeTruthy();
+    expect(baseElement).toBeInTheDocument();
+  });
+
+  it('should render the e-wallet purposes', () => {
+    render(<OnboardingStepC />);
+    const purposes = [
+      'Online shopping',
+      'Peer-to-peer payments',
+      'Investment',
+      'Bills payment',
+      'Others',
+    ];
+
+    purposes.forEach((purpose) => {
+      const purposeLabel = screen.getByLabelText(purpose);
+      expect(purposeLabel).toBeInTheDocument();
+    });
+  });
+
+  it('should render the next and back buttons', () => {
+    render(<OnboardingStepC />);
+    const backButton = screen.getByRole('button', { name: 'Back' });
+    expect(backButton).toBeInTheDocument();
+    const finishButton = screen.getByRole('button', { name: 'Finish' });
+    expect(finishButton).toBeInTheDocument();
+  });
+
+  it('should submit the form with selected purposes', async () => {
+    render(<OnboardingStepC />);
+    const purposes = [
+      'Online shopping',
+      'Peer-to-peer payments',
+      'Investment',
+      'Bills payment',
+      'Others',
+    ];
+
+    purposes.forEach((purpose, index) => {
+      if (index % 2 === 0) {
+        const purposeLabel = screen.getByLabelText(purpose);
+        userEvent.click(purposeLabel);
+      }
+    });
+
+    const finishButton = screen.getByRole('button', { name: 'Finish' });
+    fireEvent.click(finishButton);
+
+    // Replace the following line with a test that checks if the form submission was successful
+    // For example, check if the next step is rendered, or if the store has been updated with the submitted data
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // Wait for a second before checking the result
   });
 });
