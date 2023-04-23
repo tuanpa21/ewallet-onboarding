b2258b0a8ad9232fc130378ffb058ea71924a008
Pham Anh Tuan <phamtuan210394@gmail.com>
2023-04-23 11:15:17 +0700

    Update review layout

--- libs/onboarding/container/src/lib/onboarding-container.tsx
+++ libs/onboarding/container/src/lib/onboarding-container.tsx
@@ -11,6 +11,7 @@ import {
 } from '@mui/material';
 import { steps } from '@ewallet-onboarding/onboarding/config';
 import { useOnboardingStore } from '@ewallet-onboarding/onboarding/store';
+import LoadingScreen from '@ewallet-onboarding/onboarding/loading';
 
 const ReviewScreen = React.lazy(
   () => import('@ewallet-onboarding/onboarding/review')
@@ -22,69 +23,59 @@ const SuccessScreen = React.lazy(
 export function OnboardingContainer() {
   const { activeStep, showConfirm, showSuccess } = useOnboardingStore();
 
-  if (showSuccess) {
+  const renderContent = () => {
+    if (showSuccess) {
+      return <SuccessScreen />;
+    }
+    if (showConfirm) {
+      return <ReviewScreen />;
+    }
     return (
-      <Suspense fallback={<div>Loading...</div>}>
-        <SuccessScreen />
-      </Suspense>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        sx={{ background: '#efeff1', padding: '2rem', minHeight: '100vh' }}
+      >
+        <Card sx={{ minWidth: '60%', padding: '2rem' }}>
+          <CardContent>
+            <Typography variant="h4" align="center">
+              E-Wallet Onboarding
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              align="center"
+              gutterBottom
+              sx={{ marginBottom: '1rem' }}
+            >
+              Thank you for registering! Let's get started with the onboarding
+              process.
+            </Typography>
+            <Stepper activeStep={activeStep} alternativeLabel>
+              {steps.map((step, index) => (
+                <Step key={index}>
+                  <StepLabel>{step.label}</StepLabel>
+                </Step>
+              ))}
+            </Stepper>
+            <Typography
+              variant="subtitle1"
+              align="center"
+              gutterBottom
+              sx={{ marginTop: '1rem' }}
+            >
+              {steps[activeStep].description}
+            </Typography>
+            <StepComponent />
+          </CardContent>
+        </Card>
+      </Box>
     );
-  }
-
-  if (showConfirm) {
-    return (
-      <Suspense fallback={<div>Loading...</div>}>
-        <ReviewScreen />
-      </Suspense>
-    );
-  }
+  };
 
   const StepComponent = steps[activeStep].component;
 
-  return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      sx={{ background: '#efeff1', padding: '2rem', minHeight: '100vh' }}
-    >
-      <Card sx={{ minWidth: '60%', padding: '2rem' }}>
-        <CardContent>
-          <Typography variant="h4" align="center">
-            E-Wallet Onboarding
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            align="center"
-            gutterBottom
-            sx={{ marginBottom: '1rem' }}
-          >
-            Thank you for registering! Let's get started with the onboarding
-            process.
-          </Typography>
-
-          <Stepper activeStep={activeStep} alternativeLabel>
-            {steps.map((step, index) => (
-              <Step key={index}>
-                <StepLabel>{step.label}</StepLabel>
-              </Step>
-            ))}
-          </Stepper>
-          <Typography
-            variant="subtitle1"
-            align="center"
-            gutterBottom
-            sx={{ marginTop: '1rem' }}
-          >
-            {steps[activeStep].description}
-          </Typography>
-          {/* ... (StepComponent, Suspense, and Buttons) */}
-          <Suspense fallback={<div>Loading...</div>}>
-            <StepComponent />
-          </Suspense>
-        </CardContent>
-      </Card>
-    </Box>
-  );
+  return <Suspense fallback={<LoadingScreen />}>{renderContent()}</Suspense>;
 }
 
 export default OnboardingContainer;
--- libs/onboarding/review/src/lib/onboarding-review.tsx
+++ libs/onboarding/review/src/lib/onboarding-review.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Card, CardContent, Typography } from '@mui/material';
 import { useOnboardingStore } from '@ewallet-onboarding/onboarding/store';
 
 export function OnboardingReview() {
@@ -11,38 +11,60 @@ export function OnboardingReview() {
       flexDirection="column"
       justifyContent="center"
       alignItems="center"
-      sx={{ background: '#efeff1', padding: '2rem', minHeight: '100vh' }}
+      sx={{
+        background: '#efeff1',
+        padding: '2rem',
+        minHeight: '100vh',
+        minWidth: '60%',
+      }}
     >
-      <Typography variant="h4" align="center">
+      <Typography variant="h4" align="center" gutterBottom>
         Review Your Information
       </Typography>
-      <Box>
-        <Typography variant="h6">Step A</Typography>
-        <Typography>Full Name: {stepA.fullName}</Typography>
-        <Typography>ID Number: {stepA.idNumber}</Typography>
-      </Box>
-      <Box>
-        <Typography variant="h6">Step B</Typography>
-        <Typography>Email: {stepB.email}</Typography>
-        <Typography>Phone Number: {stepB.phoneNumber}</Typography>
-        <Typography>Date of Birth: {stepB.dateOfBirth}</Typography>
-      </Box>
-      <Box>
-        <Typography variant="h6">Step C</Typography>
-        <Typography>Purposes:</Typography>
-        <ul>
-          {stepC.purposes.map((purpose, index) => (
-            <li key={index}>{purpose}</li>
-          ))}
-        </ul>
-      </Box>
-      <Box>
-        <Button variant="outlined" onClick={onBack}>
-          Back
-        </Button>
-        <Button variant="contained" onClick={onComplete}>
-          Confirm
-        </Button>
+      <Box sx={{ width: '50%', margin: '0 auto' }}>
+        <Card sx={{ width: '100%', marginBottom: '2rem' }}>
+          <CardContent>
+            <Typography variant="h6">Basic Information</Typography>
+            <Typography>Full Name: {stepA.fullName}</Typography>
+            <Typography>ID Number: {stepA.idNumber}</Typography>
+          </CardContent>
+        </Card>
+        <Card sx={{ width: '100%', marginBottom: '2rem' }}>
+          <CardContent>
+            <Typography variant="h6">Additional Information</Typography>
+            <Typography>Email: {stepB.email}</Typography>
+            <Typography>Phone Number: {stepB.phoneNumber}</Typography>
+            <Typography>Date of Birth: {stepB.dateOfBirth}</Typography>
+          </CardContent>
+        </Card>
+        <Card sx={{ width: '100%', marginBottom: '2rem' }}>
+          <CardContent>
+            <Typography variant="h6">Purposes:</Typography>
+            <ul style={{ margin: 0 }}>
+              {stepC.purposes.map((purpose, index) => (
+                <li key={index}>{purpose}</li>
+              ))}
+            </ul>
+          </CardContent>
+        </Card>
+        <Box display="flex">
+          <Button
+            variant="outlined"
+            onClick={onBack}
+            sx={{ marginRight: '1rem' }}
+            fullWidth
+          >
+            Back
+          </Button>
+          <Button
+            variant="contained"
+            onClick={onComplete}
+            sx={{ backgroundColor: '#1976d2', color: '#ffffff' }}
+            fullWidth
+          >
+            Confirm
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
